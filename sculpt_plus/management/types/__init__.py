d935b158340db03eaa30f827be5f9072e8ec9e51
J. Fran Matheu G <45881831+jfranmatheu@users.noreply.github.com>
2023-07-14 11:01:42 +0200

    Remove Sculpt+ management implementation in favour of brush_manager addon

--- sculpt_plus/management/types/__init__.py
+++
@@ -1,4 +0,0 @@
-from .brush import Brush
-from .texture import Texture
-from . fake_item import FakeViewItem, FakeViewItem_Brush, FakeViewItem_Texture, FakeViewItemThumbnail
-from . cat import TextureCategory, BrushCategory
