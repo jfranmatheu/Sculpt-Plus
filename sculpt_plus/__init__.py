61adf3e88b509b82bb7bde57c726c752a39c6a5b
J. Fran Matheu G <45881831+jfranmatheu@users.noreply.github.com>
2023-08-21 11:56:48 +0200

    increment version

--- sculpt_plus/__init__.py
+++ sculpt_plus/__init__.py
@@ -16,7 +16,7 @@ bl_info = {
     "author" : "J. Fran Matheu (@jfranmatheu)",
     "description" : "",
     "blender" : (3, 4, 1),
-    "version" : (1, 0, 8),
+    "version" : (1, 1, 0),
     "location" : "Topbar [S+] button > 'Sculpt+' WorkSpace",
     "warning" : "BETA VERSION! May be unstable!",
     "category" : "General"
