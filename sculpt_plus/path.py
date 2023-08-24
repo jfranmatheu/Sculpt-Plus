bc5c65d221fe773a47a771ab6135dea078e554c0
J. Fran Matheu G <45881831+jfranmatheu@users.noreply.github.com>
2023-08-24 15:43:41 +0200

    cleanup old paths and thumbnails and pickle DB Shelve stuff

--- sculpt_plus/path.py
+++ sculpt_plus/path.py
@@ -1,11 +1,14 @@
-import os
-from os.path import join, dirname, abspath, exists as path_exists, isfile
+import bpy
+from os.path import join, dirname, abspath
 import sys
 import pathlib
-from pathlib import Path
-import shelve
-import pickle
 from enum import Enum
+
+
+b3d_user_path = bpy.utils.resource_path('USER')
+b3d_config_path = join(b3d_user_path, "config")
+
+b3d_appdata_path = dirname(bpy.utils.resource_path('USER'))
 
 
 def get_datadir() -> pathlib.Path:
@@ -27,8 +30,10 @@ def get_datadir() -> pathlib.Path:
     elif sys.platform == "darwin":
         return home / "Library/Application Support/Blender" ## /Users/$USER/Library/Application Support/Blender/3.4/
 
-addon_data_dir = get_datadir() / "addon_data"
-app_dir = addon_data_dir / __package__
+
+src_path = pathlib.Path(__file__).parent
+app_dir = pathlib.Path(b3d_appdata_path) / "addon_data" / __package__
+
 temp_dir = app_dir / "temp"
 data_dir = app_dir / "data"
 data_brush_dir = data_dir / "brush"
@@ -53,16 +58,7 @@ def ensure_paths():
             file.write(f"{bl_info['version']}\n")
 
         data_dir.mkdir(parents=False)
-        # data_brush_dir.mkdir()
-        # data_texture_dir.mkdir()
         temp_dir.mkdir()
-        # (data_brush_dir / "settings" / "defaults").mkdir(parents=True)
-        # (data_brush_dir / "cats").mkdir()
-        # (data_brush_dir / "previews").mkdir()
-        # (data_texture_dir / "settings" / "defaults").mkdir(parents=True)
-        # (data_texture_dir / "cats").mkdir()
-        # (data_texture_dir / "previews").mkdir()
-        # (data_texture_dir / "images").mkdir()
         (temp_dir / "fake_items").mkdir()
         (temp_dir / "thumbnails").mkdir()
         if not management_config_file.exists():
@@ -97,21 +93,6 @@ class SculptPlusPaths(Enum):
     CONFIG_FILE = data_dir / 'config.json'
     HOTBAR_DATA = data_dir / "hotbar"
 
-    # DATA_BRUSH_SETTINGS = data_brush_dir / "settings"
-    # DATA_BRUSH_DEFAULTS = data_brush_dir / "settings" / "defaults"
-    # DATA_BRUSH_CATS = data_brush_dir / "cats"
-    # DATA_BRUSH_PREVIEWS = data_brush_dir / "previews"
-    # DATA_BRUSH_CAT_ICONS = data_brush_dir / "cats"
-
-    # DATA_TEXTURE_SETTINGS = data_texture_dir / "settings"
-    # # DATA_TEXTURE_DEFAULTS = str(data_texture_dir / "settings" / "default")
-    # DATA_TEXTURE_CATS = data_texture_dir / "cats"
-    # DATA_TEXTURE_PREVIEWS = data_texture_dir / "previews"
-    # DATA_TEXTURE_IMAGES = data_texture_dir / "images"
-    # DATA_TEXTURE_CAT_ICONS = data_texture_dir / "cats"
-
-    TEMP_FAKE_ITEMS = temp_dir / "fake_items"
-    TEMP_THUMBNAILS = temp_dir / "thumbnails"
 
     def __call__(self, *path, as_path: bool = False):
         if not path:
@@ -121,196 +102,3 @@ class SculptPlusPaths(Enum):
     def read(self, *path) -> str:
         with open(self(*path), mode='r') as f:
             return f.read()
-
-
-class ScriptPaths:
-    GENERATE_THUMBNAILS = SculptPlusPaths.SRC_LIB_SCRIPTS('generate_thumbnails.py')
-    GENERATE_NPZ_FROM_BLENDLIB = SculptPlusPaths.SRC_LIB_SCRIPTS('generate_images_n_thumbnails.py')
-    EXPORT_BRUSHES_FROM_BLENDLIB = SculptPlusPaths.SRC_LIB_SCRIPTS('export_brushes_from_blendlib.py')
-    CONVERT_TEXTURES_TO_PNG_FROM_BLENDLIB = SculptPlusPaths.SRC_LIB_SCRIPTS('convert_textures_to_png_from_blendlib.py')
-    EXPORT_TEXTURES_FROM_DIRECTORY = SculptPlusPaths.SRC_LIB_SCRIPTS('export_textures_from_directory.py')
-    GENERATE_NPY_FROM_IMAGE_PATHS = SculptPlusPaths.SRC_LIB_SCRIPTS('generate_npy_from_image_paths.py')
-
-
-class ThumbnailPaths:
-    @staticmethod
-    def _GET_PATH(_path: SculptPlusPaths, item: str, ext: str = '.png', check_exists: bool = False) -> Path or str:
-        item_id: str = item if isinstance(item, str) else item.id
-        filepath: str = _path(item_id + ext)
-        if not check_exists:
-            return filepath
-        path: Path = Path(filepath)
-        return path if path.exists() and path.is_file() else None
-
-    @classmethod
-    def BRUSH(cls, brush, check_exists: bool = False) -> Path or None:
-        return cls._GET_PATH(SculptPlusPaths.DATA_BRUSH_PREVIEWS, brush, ext='.png', check_exists=check_exists)
-
-    @classmethod
-    def TEXTURE(cls, texture, check_exists: bool = False) -> Path or None:
-        return cls._GET_PATH(SculptPlusPaths.DATA_TEXTURE_PREVIEWS, texture, ext='.png', check_exists=check_exists)
-
-    @classmethod
-    def BRUSH_CAT(cls, cat, check_exists: bool = False) -> Path or None:
-        return cls._GET_PATH(SculptPlusPaths.DATA_BRUSH_CAT_ICONS, cat, ext='.png', check_exists=check_exists)
-
-    @classmethod
-    def TEXTURE_CAT(cls, cat, check_exists: bool = False) -> Path or None:
-        return cls._GET_PATH(SculptPlusPaths.DATA_TEXTURE_CAT_ICONS, cat, ext='.png', check_exists=check_exists)
-
-    @classmethod
-    def get_texture_image_path(cls, texture, check_exists: bool = False) -> Path or None:
-        return cls._GET_PATH(SculptPlusPaths.DATA_TEXTURE_CAT_ICONS, texture, ext=texture.image.ext, check_exists=check_exists)
-
-    @classmethod
-    def remove_brush_previews(cls, brush_ids: list[str]) -> None:
-        for brush_id in brush_ids:
-            if path := cls.BRUSH(brush_id):
-                path.unlink()
-
-    @classmethod
-    def remove_texture_previews(cls, tex_ids: list[str]) -> None:
-        for tex_id in tex_ids:
-            if path := cls.TEXTURE(tex_id):
-                path.unlink()
-
-
-class DBPickle(Enum):
-    BRUSH_SETTINGS = SculptPlusPaths.DATA_BRUSH_SETTINGS
-    BRUSH_DEFAULTS = SculptPlusPaths.DATA_BRUSH_DEFAULTS
-    BRUSH_CAT = SculptPlusPaths.DATA_BRUSH_CATS
-    TEXTURE_CAT = SculptPlusPaths.DATA_TEXTURE_CATS
-    TEXTURES = SculptPlusPaths.DATA_TEXTURE_SETTINGS
-
-    def load(self, item):
-        item_id: str = item if isinstance(item, str) else item.id
-        path: str = self.value(item_id)
-        with open(path, 'rb') as f:
-            return pickle.load(f)
-    
-    def write(self, item):
-        path: str = self.value(item.id)
-        with open(path, 'wb') as f:
-            pickle.dump(item, f)
-
-    def remove(self, item):
-        item_id: str = item if isinstance(item, str) else item.id
-        path: str = self.value(item_id)
-        os.remove(path)
-
-
-
-class DBShelfPaths:
-    BRUSH_SETTINGS = SculptPlusPaths.APP__DATA('brush_settings')
-    BRUSH_DEFAULTS = SculptPlusPaths.APP__DATA('brush_defaults')
-    BRUSH_CAT = SculptPlusPaths.APP__DATA('brush_cats')
-    TEXTURE_CAT = SculptPlusPaths.APP__DATA('texture_cats')
-    TEXTURES = SculptPlusPaths.APP__DATA('textures')
-
-    TEMPORAL = SculptPlusPaths.APP__TEMP('temporal_items')
-
-
-class DBShelf(Enum):
-    BRUSH_SETTINGS = DBShelfPaths.BRUSH_SETTINGS
-    BRUSH_DEFAULTS = DBShelfPaths.BRUSH_DEFAULTS
-    BRUSH_CAT = DBShelfPaths.BRUSH_CAT
-    TEXTURE_CAT = DBShelfPaths.TEXTURE_CAT
-    TEXTURES = DBShelfPaths.TEXTURES
-
-    TEMPORAL = DBShelfPaths.TEMPORAL
-
-    def destroy(self) -> None:
-        Path(self.value).unlink()
-
-    def items(self) -> dict:
-        path: str = self.value # item.id
-        with shelve.open(path) as db:
-            return db.items()
-    
-    def values(self) -> list:
-        path: str = self.value # item.id
-        with shelve.open(path) as db:
-            return list(db.values())
-
-    def write(self, *items: tuple):
-        path: str = self.value# item.id
-        with shelve.open(path) as db:
-            for item in items:
-                db[item.id] = item
-
-    def remove(self, *items: tuple[str]):
-        path: str = self.value# item_id
-        with shelve.open(path) as db:
-            for item in items:
-                item_id: str = item if isinstance(item, str) else item.id
-                del db[item_id]
-
-    def reset(self) -> None:
-        """ Remove everything from this database. """
-        db = shelve.open(self.value, flag='n')
-        db.close()
-
-
-class DBShelfManager:
-    @classmethod
-    def TEMPORAL(cls, cleanup: bool = True) -> 'DBShelfManager':
-        return cls(DBShelfPaths.TEMPORAL, cleanup=cleanup)
-
-    @classmethod
-    def BRUSH_SETTINGS(cls) -> 'DBShelfManager':
-        return cls(DBShelfPaths.BRUSH_SETTINGS)
-
-    @classmethod
-    def BRUSH_DEFAULTS(cls) -> 'DBShelfManager':
-        return cls(DBShelfPaths.BRUSH_DEFAULTS)
-
-    @classmethod
-    def BRUSH_CAT(cls) -> 'DBShelfManager':
-        return cls(DBShelfPaths.BRUSH_CAT)
-
-    @classmethod
-    def TEXTURE_CAT(cls) -> 'DBShelfManager':
-        return cls(DBShelfPaths.TEXTURE_CAT)
-
-    @classmethod
-    def TEXTURE(cls) -> 'DBShelfManager':
-        return cls(DBShelfPaths.TEXTURES)
-
-    def __init__(self, path: str, cleanup: bool = False):
-        self.db_path = path
-
-        if cleanup:
-            dat_filepath = Path(path + '.dat')
-            if dat_filepath.exists() and dat_filepath.is_file():
-                dat_filepath.unlink()
-            dir_filepath = Path(path + '.dir')
-            if dir_filepath.exists() and dir_filepath.is_file():
-                dir_filepath.unlink()
-            bak_filepath = Path(path + '.bak')
-            if bak_filepath.exists() and bak_filepath.is_file():
-                bak_filepath.unlink()
-
-    def __enter__(self):
-        self.db = shelve.open(self.db_path)
-        return self
-
-    def get(self, key: str):
-        return self.db[key]
-
-    def write(self, component):
-        self.db[component.id] = component
-
-    def remove(self, component):
-        item_id: str = component if isinstance(component, str) else component.id
-        del self.db[item_id]
-
-    def get_items(self) -> list:
-        return list(self.db.items())
-
-    def __exit__(self, exc_type, exc_value, exc_traceback):
-        self.db.close()
-
-    def reset(self) -> None:
-        """ Remove everything from this database. """
-        self.db.close()
-        self.db = shelve.open(self.db_path, flag='n')
