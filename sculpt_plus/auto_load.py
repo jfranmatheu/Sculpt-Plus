34822a58d8dc2c469f33fd8420c450417f16585e
jfranmatheu <45881831+jfranmatheu@users.noreply.github.com>
2025-01-29 17:04:15 +0100

    update auto-load

--- sculpt_plus/auto_load.py
+++ sculpt_plus/auto_load.py
@@ -1,12 +1,11 @@
+import os
 import bpy
+import sys
 import typing
 import inspect
 import pkgutil
 import importlib
 from pathlib import Path
-import sys
-import importlib
-
 
 __all__ = (
     "init",
@@ -14,210 +13,217 @@ __all__ = (
     "unregister",
 )
 
+blender_version = bpy.app.version
 
-if __package__ == 'sculpt_plus':
-    blender_version = bpy.app.version
+modules = None
+ordered_classes = None
 
-    modules = None
-    ordered_classes = None
+def init():
+    global modules
+    global ordered_classes
 
-    def init(USE_DEV_ENVIRONMENT):
-        global modules
-        global ordered_classes
+    modules = get_all_submodules(Path(__file__).parent)
+    ordered_classes = get_ordered_classes_to_register(modules)
 
-        modules = get_all_submodules(Path(__file__).parent, USE_DEV_ENVIRONMENT)
-        ordered_classes = get_ordered_classes_to_register(modules)
+    for module in modules:
+        if module.__name__ == __name__:
+            continue
+        if hasattr(module, "init_pre"):
+            module.init_pre()
 
-    def register():
-        print("[Sculpt+] Registering...")
-        global modules
-        global ordered_classes
-        #from sculpt_plus.sculpt_hotbar.reg import get_classes
+    for module in modules:
+        if module.__name__ == __name__:
+            continue
+        if hasattr(module, "init"):
+            module.init()
 
-        # When installing new version over... it tends to not run the init LMAO.
-        if ordered_classes is None:
-            init(False)
-            print("init modules and classes...")
-
-        for cls in ordered_classes:
-            bpy.utils.register_class(cls)
-
-        for module in modules:
-            if module.__name__ == __name__:
-                continue
-            if hasattr(module, "pre_register"):
-                module.pre_register()
-
-        for module in modules:
-            if module.__name__ == __name__:
-                continue
-            if hasattr(module, "register"):
-                module.register()
-
-        for module in modules:
-            if module.__name__ == __name__:
-                continue
-            if hasattr(module, "post_register"):
-                module.post_register()
-
-        #for cls in get_classes():
-        #    bpy.utils.register_class(cls)
-
-    def unregister():
-        print("[Sculpt+] Unregistering...")
-        global modules
-        global ordered_classes
-        #from sculpt_plus.sculpt_hotbar.reg import get_classes
-
-        #for cls in get_classes():
-        #    bpy.utils.unregister_class(cls)
-
-        for cls in reversed(ordered_classes):
-            #### print("[Sculpt+] Unregistering class %s" % cls.__name__)
-            bpy.utils.unregister_class(cls)
-
-        for module in modules:
-            if module.__name__ == __name__:
-                continue
-            if hasattr(module, "pre_unregister"):
-                module.pre_unregister()
-
-        #print(__name__)
-        for module in modules:
-            if module.__name__ == __name__:
-                continue
-            #### print("[Sculpt+] Unregistering module %s" % module.__name__)
-            if hasattr(module, "unregister"):
-                module.unregister()
-
-        for module in modules:
-            if module.__name__ == __name__:
-                continue
-            if hasattr(module, "post_unregister"):
-                module.post_unregister()
-
-        sys_modules = sys.modules
-        for module in modules:
-            if module == __name__:
-                continue
-            if module.__name__ in sys_modules:
-                ## print("[Sculpt+] Removing module %s" % module.__name__)
-                del sys.modules[module.__name__]
-
-        if __name__ in sys_modules:
-            del sys.modules[__name__]
+    for module in modules:
+        if module.__name__ == __name__:
+            continue
+        if hasattr(module, "init_post"):
+            module.init_post()
 
 
-    # Import modules
-    #################################################
+def register():
+    for cls in ordered_classes:
+        bpy.utils.register_class(cls)
 
-    def get_all_submodules(directory, USE_DEV_ENVIRONMENT):
-        return list(iter_submodules(directory, directory.name, USE_DEV_ENVIRONMENT))
+    for module in modules:
+        if module.__name__ == __name__:
+            continue
+        if hasattr(module, "register_pre"):
+            module.register_pre()
 
-    def iter_submodules(path, package_name, USE_DEV_ENVIRONMENT):
-        #print(sys.modules.keys())
-        for name in sorted(iter_submodule_names(path)):
-            full_name = __package__ + '.' + name
-            # print(full_name)
-            if not USE_DEV_ENVIRONMENT and full_name in sys.modules:
-                #print("\t - reload")
-                yield importlib.reload(sys.modules[full_name])
-            else:
-                #print("\t - import")
-                yield importlib.import_module(full_name) # "." + name, package_name)
+    for module in modules:
+        if module.__name__ == __name__:
+            continue
+        if hasattr(module, "register"):
+            module.register()
 
-    def iter_submodule_names(path, root=""):
-        for _, module_name, is_package in pkgutil.iter_modules([str(path)]):
-            if is_package:
-                sub_path = path / module_name
-                sub_root = root + module_name + "."
-                yield from iter_submodule_names(sub_path, sub_root)
-            else:
-                yield root + module_name
+    for module in modules:
+        if module.__name__ == __name__:
+            continue
+        if hasattr(module, "register_post"):
+            module.register_post()
+
+    from .ackit.globals import GLOBALS
+    if GLOBALS.check_in_development():
+        print("[Sculpt+] Generating types, ops and icons...")
+        from .ackit._auto_code_gen import AddonCodeGen
+        AddonCodeGen.TYPES(types_alias='splus')
+        AddonCodeGen.OPS()
+        AddonCodeGen.ICONS()
 
 
-    # Find classes to register
-    #################################################
+def unregister():
+    for cls in reversed(ordered_classes):
+        bpy.utils.unregister_class(cls)
 
-    def get_ordered_classes_to_register(modules):
-        return toposort(get_register_deps_dict(modules))
+    for module in modules:
+        if module.__name__ == __name__:
+            continue
+        if hasattr(module, "unregister_pre"):
+            module.unregister_pre()
 
-    def get_register_deps_dict(modules):
-        my_classes = set(iter_my_classes(modules))
-        my_classes_by_idname = {cls.bl_idname : cls for cls in my_classes if hasattr(cls, "bl_idname")}
+    for module in modules:
+        if module.__name__ == __name__:
+            continue
+        if hasattr(module, "unregister"):
+            module.unregister()
 
-        deps_dict = {}
-        for cls in my_classes:
-            deps_dict[cls] = set(iter_my_register_deps(cls, my_classes, my_classes_by_idname))
-        return deps_dict
+    for module in modules:
+        if module.__name__ == __name__:
+            continue
+        if hasattr(module, "unregister_post"):
+            module.unregister_post()
 
-    def iter_my_register_deps(cls, my_classes, my_classes_by_idname):
-        yield from iter_my_deps_from_annotations(cls, my_classes)
-        yield from iter_my_deps_from_parent_id(cls, my_classes_by_idname)
 
-    def iter_my_deps_from_annotations(cls, my_classes):
-        for value in typing.get_type_hints(cls, {}, {}).values():
-            dependency = get_dependency_from_annotation(value)
-            if dependency is not None:
-                if dependency in my_classes:
-                    yield dependency
+# Import modules
+#################################################
 
-    def get_dependency_from_annotation(value):
-        if blender_version >= (2, 93):
-            if isinstance(value, bpy.props._PropertyDeferred):
-                return value.keywords.get("type")
+def get_all_submodules(directory):
+    return list(iter_submodules(directory, __package__))
+
+def iter_submodules(path, package_name):
+    for name in sorted(iter_submodule_names(path)):
+        yield importlib.import_module("." + name, package_name)
+
+def iter_submodule_names(path, root=""):
+    for _, module_name, is_package in pkgutil.iter_modules([str(path)]):
+        if is_package:
+            sub_path = path / module_name
+            sub_root = root + module_name + "."
+            yield from iter_submodule_names(sub_path, sub_root)
         else:
-            if isinstance(value, tuple) and len(value) == 2:
-                if value[0] in (bpy.props.PointerProperty, bpy.props.CollectionProperty):
-                    return value[1]["type"]
-        return None
-
-    def iter_my_deps_from_parent_id(cls, my_classes_by_idname):
-        if bpy.types.Panel in cls.__bases__:
-            parent_idname = getattr(cls, "bl_parent_id", None)
-            if parent_idname is not None:
-                parent_cls = my_classes_by_idname.get(parent_idname)
-                if parent_cls is not None:
-                    yield parent_cls
-
-    def iter_my_classes(modules):
-        base_types = get_register_base_types()
-        for cls in get_classes_in_modules(modules):
-            if any(base in base_types for base in cls.__bases__):
-                if not getattr(cls, "is_registered", False):
-                    yield cls
-
-    def get_classes_in_modules(modules):
-        classes = set()
-        for module in modules:
-            for cls in iter_classes_in_module(module):
-                classes.add(cls)
-        return classes
-
-    def iter_classes_in_module(module):
-        for value in module.__dict__.values():
-            if inspect.isclass(value):
-                yield value
-
-    def get_register_base_types():
-        return set(getattr(bpy.types, name) for name in [
-            "UIList","Panel","PropertyGroup","AddonPreferences","Gizmo","GizmoGroup","Operator"
-        ])
+            yield root + module_name
 
 
-    # Find order to register to solve dependencies
-    #################################################
+# Find classes to register
+#################################################
 
-    def toposort(deps_dict):
-        sorted_list = []
-        sorted_values = set()
-        while len(deps_dict) > 0:
-            unsorted = []
-            for value, deps in deps_dict.items():
-                if len(deps) == 0:
-                    sorted_list.append(value)
-                    sorted_values.add(value)
-                else:
-                    unsorted.append(value)
-            deps_dict = {value : deps_dict[value] - sorted_values for value in unsorted}
-        return sorted_list
+def get_ordered_classes_to_register(modules):
+    return toposort(get_register_deps_dict(modules))
+
+def get_register_deps_dict(modules):
+    my_classes = set(iter_my_classes(modules))
+    my_classes_by_idname = {cls.bl_idname : cls for cls in my_classes if hasattr(cls, "bl_idname")}
+
+    deps_dict = {}
+    for cls in my_classes:
+        deps_dict[cls] = set(iter_my_register_deps(cls, my_classes, my_classes_by_idname))
+    return deps_dict
+
+def iter_my_register_deps(cls, my_classes, my_classes_by_idname):
+    yield from iter_my_deps_from_annotations(cls, my_classes)
+    yield from iter_my_deps_from_parent_id(cls, my_classes_by_idname)
+
+def iter_my_deps_from_annotations(cls, my_classes):
+    for value in typing.get_type_hints(cls, {}, {}).values():
+        dependency = get_dependency_from_annotation(value)
+        if dependency is not None:
+            if dependency in my_classes:
+                yield dependency
+
+def get_dependency_from_annotation(value):
+    if isinstance(value, bpy.props._PropertyDeferred):
+        return value.keywords.get("type")
+        prop_type: str = value.function.__name__
+        prop_type = prop_type.replace('Property', '').upper()
+        if prop_type in {'POINTER', 'COLLECTION'}:
+            type = value.keywords.get('type')
+            attr_name = value.keywords.get('attr')
+            return type
+    return None
+
+def iter_my_deps_from_parent_id(cls, my_classes_by_idname):
+    if issubclass(cls, bpy.types.Panel):
+        parent_idname = getattr(cls, "bl_parent_id", None)
+        if parent_idname is not None:
+            parent_cls = my_classes_by_idname.get(parent_idname)
+            if parent_cls is not None:
+                yield parent_cls
+
+def iter_my_classes(modules):
+    base_types = get_register_base_types()
+    for cls in get_classes_in_modules(modules):
+        if any(issubclass(cls, base) for base in base_types):
+            if not getattr(cls, "is_registered", False):
+                yield cls
+
+def get_classes_in_modules(modules):
+    classes = set()
+    for module in modules:
+        for cls in iter_classes_in_module(module):
+            classes.add(cls)
+    return classes
+
+def iter_classes_in_module(module):
+    for value in module.__dict__.values():
+        if inspect.isclass(value):
+            yield value
+
+def get_register_base_types():
+    return set(getattr(bpy.types, name) for name in [
+        "Panel", "Operator",
+        "Header", "Menu",
+        "UIList",
+    ])
+
+
+# Find order to register to solve dependencies
+#################################################
+
+def toposort(deps_dict):
+    sorted_list = []
+    sorted_values = set()
+    while len(deps_dict) > 0:
+        unsorted = []
+        for value, deps in deps_dict.items():
+            if len(deps) == 0:
+                sorted_list.append(value)
+                sorted_values.add(value)
+            else:
+                unsorted.append(value)
+        deps_dict = {value : deps_dict[value] - sorted_values for value in unsorted}
+    return sorted_list
+
+def get_ordered_pg_classes_to_register(classes) -> list:
+    my_classes = set(classes)
+    deps_dict = {}
+
+    for cls in my_classes:
+        deps_dict[cls] = set(
+            iter_my_deps_from_annotations(cls, my_classes)
+        )
+
+    return toposort(deps_dict)
+
+
+# CLASS GETTERS.
+
+def get_pg_classes():
+    global ordered_classes
+    return [
+        cls for cls in ordered_classes
+        if issubclass(cls, bpy.types.PropertyGroup)
+    ]
