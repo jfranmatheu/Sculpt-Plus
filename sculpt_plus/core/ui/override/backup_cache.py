b8f11e5ffd6e208f4e0a0b041985d3a1eb994f25
jfranmatheu <45881831+jfranmatheu@users.noreply.github.com>
2025-01-30 17:05:37 +0100

    fix backup cache for override UI

--- sculpt_plus/core/ui/override/backup_cache.py
+++ sculpt_plus/core/ui/override/backup_cache.py
@@ -6,57 +6,56 @@ from bpy.types import (
 )
 from collections import defaultdict
 
-
-classes_to_cache = (
-    VIEW3D_PT_tools_active,
-    VIEW3D_HT_tool_header,
-    VIEW3D_PT_tools_brush_select,
-    VIEW3D_PT_tools_brush_settings
-)
-
 _cache_reset = {}
 _mod_cls_attributes = defaultdict(set)
 
 
 def get_attr_from_cache(cls, attr, default=None):
+    global _cache_reset
+
     if cls_cache := _cache_reset.get(cls, None):
-        if hasattr(cls_cache, attr):
-            return cls_cache[attr]
+        if value := cls_cache.get(attr, default):
+            return value
         else:
             print(f"No cache attr '{attr}' for cls '{cls}'")
     else:
         print(f"No cache for cls:", cls)
-    return default
+    if default is not None:
+        return default
+    if value := getattr(cls, attr, default):
+        return value
+    return None
 
 def cache_cls_attributes(cls) -> dict:
+    global _cache_reset
     _cache_reset[cls] = cls.__dict__.copy()
     return _cache_reset[cls]
 
 
 def set_cls_attribute(cls, attr: str, new_value):
     ## print("CLS ATTR:", cls, attr, getattr(cls, attr))
+    global _mod_cls_attributes
+    global _cache_reset
 
     if cache := _cache_reset.get(cls, None):
         pass
     else:
-        cache = cache_cls_attributes(cls)
-
-    setattr(cls, attr, new_value)
+        cache = {}
 
     if attr not in cache:
-        print("Attribute %s not in cls" % attr, cls)
-        return
+        cache[attr] = cls.__dict__.copy().get(attr, None)
+        _cache_reset[cls] = cache
+
+    setattr(cls, attr, new_value)
 
     _mod_cls_attributes[cls].add(attr)
 
     setattr(cls, 'old_' + attr, cache[attr])
 
 
-def register_pre():
-    for cls in classes_to_cache:
-        cache_cls_attributes(cls)
-
 def unregister_pre():
+    global _mod_cls_attributes
+
     for cls, mod_cls_attributes in _mod_cls_attributes.items():
         cache = _cache_reset[cls] # raises AttributeError on class without decorator
         for mod_attr in mod_cls_attributes:
